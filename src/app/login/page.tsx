fe357e0b9d25d7669b27cef56c02d8c2ec63d591
Stvaer <sol.and.terms@gmail.com>
2025-07-17 20:19:48 +0000

    ya esta el deploy pero sale esto

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -166,7 +166,9 @@ export default function LoginPage() {
                 {pin.map((digit, index) => (
                   <input
                     key={index}
-                    ref={(el) => { inputRefs.current[index] = el; }}
+                    ref={(el) => {
+                      inputRefs.current[index] = el;
+                    }}
                     type="password"
                     maxLength={1}
                     className={cn(
--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,11 +1,11 @@
 
 // Import the functions you need from the SDKs you need
-import { initializeApp, getApps, getApp, FirebaseApp, FirebaseOptions } from "firebase/app";
+import { initializeApp, getApps, getApp, FirebaseApp } from "firebase/app";
 import { getFirestore, Firestore } from "firebase/firestore";
 import { getAuth, Auth } from "firebase/auth";
 
 // Your web app's Firebase configuration from environment variables
-const firebaseConfig: FirebaseOptions = {
+const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
